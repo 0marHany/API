ff286f694f7ff6eacf0a70f2728f5a6f6834e118
omarHany <omarhany028@gmail.com>
2022-06-30 15:04:08 +0200

    done

--- app.js
+++ app.js
@@ -16,9 +16,9 @@ require("dotenv").config(); // for reading the .env file
 // for creating pdf
 // var Python = require("python-runner");
 const sendEmail = require('./common/service/sendEmail');
-const { PythonShell } = require('python-shell');
+// const { PythonShell } = require('python-shell');
 // const speedsModel = require('./model/speeds.model');
-const linksModel = require('./model/links.model');
+// const linksModel = require('./model/links.model');
 // const path = require('path');
 // speedsModel
 app.use(express.json())
@@ -37,8 +37,6 @@ app.use(
 app.post('/sendEmail/:id', async (req, res) => {
     const { listEmail, content } = req.body;
     const attach = [{
-        // const myPath = './Pdf/';
-
         filename: 'STA.pdf',
         contentType: 'application/pdf',
         path: './Pdf/' + `${req.params.id}.pdf`
@@ -47,23 +45,23 @@ app.post('/sendEmail/:id', async (req, res) => {
     res.send(info);
 })
 
-const links = async (req, res) => {
-    let hamda = await linksModel.findOne({ _id: req.params.id });
-    let get = hamda._id;
-    let options = {
-        args: [`-p ${get}`]
-    };
+// const links = async (req, res) => {
+//     let hamda = await linksModel.findOne({ _id: req.params.id });
+//     let get = hamda._id;
+//     let options = {
+//         args: [`-p ${get}`]
+//     };
 
-    PythonShell.run('script.py', options, function (err, results) {
-        if (err) throw err;
-        // results is an array consisting of messages collected during execution
-        console.log('results: %j', results);
+//     PythonShell.run('script.py', options, function (err, results) {
+//         if (err) throw err;
+//         // results is an array consisting of messages collected during execution
+//         console.log('results: %j', results);
 
-        res.json(results);
-    }
-    );
-}
-app.get('/zhkt/:id', links)
+//         res.json(results);
+//     }
+//     );
+// }
+// app.get('/zhkt/:id', links)
 
 mongoose.connect(process.env.MONGOO_CONNECTION);
 app.listen(process.env.PORT || 4000)
