acc8f924eedfd77f42a48af83f533b21ffde759c
omarHany <omarhany028@gmail.com>
2022-08-11 14:02:38 +0200

    Hook

--- model/user.model.js
+++ model/user.model.js
@@ -1,6 +1,6 @@
 
 const mongoose = require('mongoose')
-
+const bcrypt = require("bcrypt");
 
 const userSchema = mongoose.Schema({
 
@@ -11,5 +11,10 @@ const userSchema = mongoose.Schema({
     timestamps: true
 })
 
+// Hash Password With Hook
+userSchema.pre('save', async function (next) {
+    this.password = await bcrypt.hash(this.password, 12);
+    next();
+});
 
 module.exports = mongoose.model('user', userSchema)
